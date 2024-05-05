0e5def75c3afe6702a5db41fd4b0f881baea367c
JuanGallego1107 <118768557+JuanGallego1107@users.noreply.github.com>
2024-05-05 17:15:19 -0500

    .

--- test/solid_app_test.dart
+++ test/solid_app_test.dart
@@ -1,16 +1,1 @@
-import 'package:solid_app/solid_app.dart';
-import 'package:test/test.dart';
 
-void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
